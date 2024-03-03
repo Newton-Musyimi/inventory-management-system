592f6c8e6cce4b15cfe9bade62e49dd7caf8f15a
Newton Musyimi <28211658+Newton-Musyimi@users.noreply.github.com>
2024-03-03 06:37:30 +0300

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            RolesAndPermissionsSeeder::class,
             UserSeeder::class,
             CategorySeeder::class,
             UnitSeeder::class,
